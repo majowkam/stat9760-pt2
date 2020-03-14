322b4c56a657d4ca22791f19c4b6120fe20afc1b
majowkam <majowkam@gmail.com>
2020-03-13 22:57:35 -0400

    project-part-2-added-temp-file-management

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from src import opcv, es
 import sys
 import os
-import requests
+from datetime import datetime as dt
 
-def main(page_size:int, num_pages:int=1, output:str='app//temp.json', esindex:str='index-violation'): 
+temp_path = "app//_temp//"
+temp_json = dt.now().strftime('%Y%m%d.%M%S') + '_temp.json'
+
+def main(page_size:int, num_pages:int=1, output:str=temp_path + temp_json, esindex:str='index-default'): 
 	opcv.get_data(apitoken, int(page_size), int(num_pages), output)
 	es.send_to_es(output, esindex)
-	
-	
-	
+
+def temp_dir(temppath):
+	if not os.path.exists(temppath):
+		os.makedirs(temppath)
+		return True
+	return False
+
 if __name__ == "__main__":
 	args   = sys.argv[1:]
+	madetempdir = temp_dir(temp_path)
 	apitoken = os.environ['APIKEY']
 	main(*args)
-	
+	if os.path.exists(temp_path + temp_json):
+		os.remove(temp_path + temp_json)
+	if madetempdir:
+		os.rmdir(temp_path)
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
-docker-compose run pyth python main.py 500 3 app/test.json matt-index-3
+docker-compose run pyth python main.py 500 3
 #docker-compose down
